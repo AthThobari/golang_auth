c6b01c0abc868ad1a0b99c8e8207113615eed480
AthThobari <thobari.mutu@gmail.com>
2025-06-14 22:10:14 +0700

    feat(auth): add verify token

--- auth1/utils/jwt.go
+++ auth1/utils/jwt.go
@@ -16,3 +16,29 @@ func GenerateToken(userID uint) (string, error) {
   token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
   return token.SignedString(secretKey)
 }
+
+/* VerifyToken verifies a token JWT validate */
+func VerifyToken(tokenString string) (jwt.MapClaims, error) {
+// parse the token
+token, err := jwt.Parse(tokenString, 
+func(token *jwt.Token) (interface{}, error) {
+  // check the signing method
+  if _, ok := token.Method.(*jwt.SigningMethodHS256); !ok {
+    return nil, fmt.Errorf("Invalid signing method")
+}
+
+return secretKey, nil
+})
+
+//check for errors
+if err != nil {
+  return nil, err
+}
+
+//validate the token
+if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+  return claims, nil
+}
+
+return nil, fmt.Errorf("Invalid token")
+}
